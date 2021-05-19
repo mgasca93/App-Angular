d5544d3c7ba040f395fcaabf8c2feb66082c4840
Mario Gasca Lopez <web@mariogasca.com>
2021-05-19 09:52:41 -0500

    Se creo el AppRoutingModule y se integro a la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
@@ -11,7 +12,8 @@ import { PagesModule } from './pages/pages.module';
   ],
   imports: [
     BrowserModule,
-    PagesModule
+    PagesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
