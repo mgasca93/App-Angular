b1746f7f05b1b1232ceb5a9fccf5624e6ca90f48
Mario Gasca Lopez <web@mariogasca.com>
2021-05-19 09:48:02 -0500

    Se creo el PagesModule para el manejo de los componentes de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
+import { PagesModule } from './pages/pages.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
